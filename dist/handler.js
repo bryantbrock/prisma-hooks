6f00d116e24d19ea4d223b0e0cc4a795da1003ac
Bryant Brock <bryant.brock@anedot.com>
2023-04-08 08:07:32 -0500

    fix(ts): build to es6 not common

--- dist/handler.js
+++ dist/handler.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,9 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handlePrismaQuery = void 0;
-const handlePrismaQuery = (params) => __awaiter(void 0, void 0, void 0, function* () {
+export const handlePrismaQuery = (params) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     try {
         const { model, action, query, db, count } = params;
@@ -39,5 +36,4 @@ const handlePrismaQuery = (params) => __awaiter(void 0, void 0, void 0, function
         return { error: message };
     }
 });
-exports.handlePrismaQuery = handlePrismaQuery;
 //# sourceMappingURL=handler.js.map
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export * from "./dist/handler";
-export * from "./dist/hooks";
+export * from "./dist/handler.js";
+export * from "./dist/hooks.js";
